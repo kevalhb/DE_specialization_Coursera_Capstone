8e3c15378a6acbe51bcd6fc3e6bcfc932d02b225
Keval Bhanushali <32277001+kevalhb@users.noreply.github.com>
2025-01-04 23:37:44 -0800

    incorrect_commit_removing

--- classicmodels_modeling/target/compiled/classicmodels_modeling/models/star_schema/dim_employees.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    md5(cast(coalesce(cast(employeeNumber as TEXT), '') as TEXT))  as employee_key,
-    lastName as employee_last_name, 
-    firstName as employee_first_name, 
-    jobTitle as job_title, 
-    email as email
-FROM classicmodels.employees
